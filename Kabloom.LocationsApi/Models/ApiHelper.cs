167c0d22f2e0c616041bb20c6c1ca035ded22c0d
Shannon Grantski <grantski@pm.me>
2021-01-25 15:03:52 -0800

    add ApiHelper

---
+++ Kabloom.LocationsApi/Models/ApiHelper.cs
@@ -0,0 +1,16 @@
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace KabloomLocationsApi.Models
+{
+  class ApiHelper
+  {
+    public static async Task<string> ApiCall(string clientId, string clientSecret)
+    {
+      RestClient client = new RestClient("https://api.foursquare.com/v2/venues/");
+      RestRequest request = new RestRequest($"search?client_id={clientId}&client_secret={clientSecret}&v=20210125&near=PORTLAND, OR&intent=browse&radius=10000&limit=1&categoryId=4eb1c0253b7b52c0e1adc2e9", Method.GET);
+      var response = await client.ExecuteTaskAsync(request);
+      return response.Content;
+    }
+  }
+}
