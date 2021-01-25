a614e7f657a5a307120365ce5558aaa14b5ef09e
Shannon Grantski <grantski@pm.me>
2021-01-25 15:03:01 -0800

    change Usings, adjust spacing

--- Kabloom.LocationsApi/Program.cs
+++ Kabloom.LocationsApi/Program.cs
@@ -1,18 +1,18 @@
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using RestSharp;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using KabloomLocationsApi.Models;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace KabloomLocationsApi
 {
-    public class Program
-    {
-        public static void Main(string[] args)
+  public class Program
+  {
+    public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
         }
@@ -22,3 +22,28 @@ namespace KabloomLocationsApi
                 .UseStartup<Startup>();
     }
 }
+
+      // CreateWebHostBuilder(args).Build().Run();
+      // var apiCallTask = ApiHelper.ApiCall(clientId, clientSecret);
+      // var result = apiCallTask.Result;
+      // JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+			// List<Location> locationList = JsonConvert.DeserializeObject<List<Location>>(jsonResponse["results"].ToString());
+
+			// foreach (Location location in locationList)
+			// {
+			// 	{location.Name};
+			// 	{location.Address};
+			// 	{location.City};
+			// 	{location.Country};
+			// 	{location.FormattedAddress};
+			// }
+			// Console.WriteLine(jsonResponse["results"]);
+    // }
+
+
+		// 	}
+    // }
+
+    // public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+    //   WebHost.CreateDefaultBuilder(args)
+    //     .UseStartup<Startup>();
