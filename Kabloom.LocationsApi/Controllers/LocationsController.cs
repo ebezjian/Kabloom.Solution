906d8c8a1beb6da87fcbf72c4870e7bf6c30886b
Shannon Grantski <grantski@pm.me>
2021-01-25 15:03:41 -0800

    delete unused code

--- Kabloom.LocationsApi/Controllers/LocationsController.cs
+++ Kabloom.LocationsApi/Controllers/LocationsController.cs
@@ -1,45 +1,15 @@
-using System.Collections.Generic;
-using System.Linq;
 using KabloomLocationsApi.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace KabloomLocationsApi.Controllers
 {
     public class LocationsController : Controller
     {
-     // GET api/locations
     [HttpGet]
-    public ActionResult<IEnumerable<string>> Get()
+    public IActionResult Index()
     {
-      return new string[] { "value1", "value2" };
+      var allLocations = Location.GetLocations(EnvironmentVariables.ClientId, EnvironmentVariables.ClientSecret);
+      return View(allLocations);
     }
-
-    // GET api/locations/
-    [HttpGet("{id}")]
-    public ActionResult<string> Get(int id)
-    {
-      return "value";
-    }
-
-    // POST api/locations
-    [HttpPost]
-    public void Post([FromBody] string value)
-    {
-      
-    }
-
-    // PUT api/locations/
-    [HttpPut("{id}")]
-    public void Put(int id, [FromBody] string value)
-    {
-    }
-
-    // DELETE api/locations/
-    [HttpDelete("{id}")]
-    public void Delete(int id)
-    {
-
-    }
-}
+  }
 }
