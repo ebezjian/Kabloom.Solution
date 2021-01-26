49e8a504b334da65561e56f26c3f7befb2a372e0
Shannon Grantski <grantski@pm.me>
2021-01-26 01:07:34 -0800

    remove unused Using statements

--- Kabloom.LocationsApi/Startup.cs
+++ Kabloom.LocationsApi/Startup.cs
@@ -1,21 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
-using Microsoft.AspNetCore.Mvc.Versioning;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
-using Swashbuckle.AspNetCore.Swagger;
 using KabloomLocationsApi.Models;
-
 
 namespace KabloomLocationsApi
 {
@@ -42,13 +30,8 @@ namespace KabloomLocationsApi
       {
         app.UseHsts();
       }
-
-      app.UseMvc(routes =>
-      {
-        routes.MapRoute(
-          name: "default",
-          template: "{controller=Home}/{action=Index}/{id?}");
-      });
+      app.UseStaticFiles();
+      app.UseMvc();
     }
   }
 }
