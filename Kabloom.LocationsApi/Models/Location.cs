3fbd4e189207ccab4380b762ae5fcb4d0962e745
Shannon Grantski <grantski@pm.me>
2021-01-25 19:09:11 -0800

    comment out code

--- Kabloom.LocationsApi/Models/Location.cs
+++ Kabloom.LocationsApi/Models/Location.cs
@@ -1,31 +1,39 @@
-using System.ComponentModel.DataAnnotations;
+using System;
 using System.Collections.Generic;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using System.ComponentModel.DataAnnotations;
 
 namespace KabloomLocationsApi.Models
 {
-    public class Location
-    {
-        public int LocationId { get; set; }
-        public string Name {get;set;}
-        public string Address {get;set;}
-        public string City {get;set;}
-        public string State {get;set;}
-        public string Country {get;set;}
-        public string FormattedAddress {get;set;}
+  public class Location
+  {
+    public int LocationId { get; set; }
+    public string Name {get;set;}
+    public string Address {get;set;}
+    public string City {get;set;}
+    public string State {get;set;}
+    public string Country {get;set;}
+    public string FormattedAddress {get;set;}
 
-        public static List<Location> GetLocations()
-        {
-            var apiCallTask = ApiHelper.ApiCall();
-            var result = apiCallTask.Result;
+    // public static List<Location> GetLocations()
+    // {
+      // var apiCallTask = ApiHelper.ApiCall();
+      // var result = apiCallTask.Result;
 
-            JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
-            
-            List<Location> locationList = JsonConvert.DeserializeObject<List<Location>>(jsonResponse["results"].ToString());
+			// Location location = new Location();
+			// location.Name = "one";
+			// location.LocationId = 7;
+			// string result = JsonConvert.SerializeObject(location);
 
-            return locationList;
-        }
+			// JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
 
-    }
+			// Console.WriteLine(jsonResponse);
+			// Console.WriteLine(jsonResponse["Name"]);
+
+			// List<Location> dummy = JsonConvert.DeserializeObject<List<Location>>(jsonResponse["response.venues"].ToString());
+			// return dummy;
+    // }
+
+  }
 }
