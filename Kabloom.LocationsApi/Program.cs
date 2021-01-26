4be5efd628bb3becdbf3338e061b5ceb1d814142
Shannon Grantski <grantski@pm.me>
2021-01-25 19:08:57 -0800

    delete unused code

--- Kabloom.LocationsApi/Program.cs
+++ Kabloom.LocationsApi/Program.cs
@@ -1,49 +1,28 @@
 using System;
-using System.Threading.Tasks;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using RestSharp;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using KabloomLocationsApi.Models;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
 
 namespace KabloomLocationsApi
 {
   public class Program
   {
     public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
-        }
+      {
+        CreateWebHostBuilder(args).Build().Run();
+      }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
-    }
+    public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+      WebHost.CreateDefaultBuilder(args)
+        .UseStartup<Startup>();
+  }
 }
-
-      // CreateWebHostBuilder(args).Build().Run();
-      // var apiCallTask = ApiHelper.ApiCall(clientId, clientSecret);
-      // var result = apiCallTask.Result;
-      // JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
-			// List<Location> locationList = JsonConvert.DeserializeObject<List<Location>>(jsonResponse["results"].ToString());
-
-			// foreach (Location location in locationList)
-			// {
-			// 	{location.Name};
-			// 	{location.Address};
-			// 	{location.City};
-			// 	{location.Country};
-			// 	{location.FormattedAddress};
-			// }
-			// Console.WriteLine(jsonResponse["results"]);
-    // }
-
-
-		// 	}
-    // }
-
-    // public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-    //   WebHost.CreateDefaultBuilder(args)
-    //     .UseStartup<Startup>();
