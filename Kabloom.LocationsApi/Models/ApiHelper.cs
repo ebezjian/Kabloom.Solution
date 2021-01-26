2e36aac9194bc7104f9db1d62895e152a7bcbd31
Shannon Grantski <grantski@pm.me>
2021-01-26 01:09:14 -0800

    break up Rests for readibility

--- Kabloom.LocationsApi/Models/ApiHelper.cs
+++ Kabloom.LocationsApi/Models/ApiHelper.cs
@@ -8,9 +8,25 @@ namespace KabloomLocationsApi.Models
   {
     public static async Task<string> ApiCall()
     {
-      RestClient client = new RestClient("https://api.foursquare.com/v2/venues/");
-      RestRequest request = new RestRequest($"search?client_id={EnvironmentVariables.ClientId}&client_secret={EnvironmentVariables.ClientSecret}&v=20210125&near=PORTLAND, OR&intent=browse&radius=10000&limit=1&categoryId=4eb1c0253b7b52c0e1adc2e9", Method.GET);
+      string url = "https://api.foursquare.com/v2/venues/";
+      string clientId = $"client_id={EnvironmentVariables.ClientId}";
+      string clientSecret = $"&client_secret={EnvironmentVariables.ClientSecret}";
+      string versionByDate = "&v=20210125";
+      // string nearLocation = $"&near={Search}";
+      string nearLocation = $"&near=Portland, OR";
+      string intent = "&intent=browse";
+      string radius = "&radius=10000";
+      string limit = "&limit=5";
+      string categoryId = "&categoryId=4eb1c0253b7b52c0e1adc2e9";
+
+      RestClient client = new RestClient(url);
+      RestRequest request = new RestRequest(
+      (
+        "search?" + clientId + clientSecret + versionByDate + nearLocation + intent + radius + limit + categoryId
+      ), Method.GET);
+
       var response = await client.ExecuteTaskAsync(request);
+
       return response.Content;
     }
   }
